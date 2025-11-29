1f9689e35155f64d43bae4c6d574a4de69ed21df
jainish2222 <jainishkoladiya22@gmail.com>
2025-11-29 15:26:10 +0530

    home page

--- src/pages/Gamepage.jsx
+++ src/pages/Gamepage.jsx
@@ -361,7 +361,7 @@ function Gamepage() {
   return (
     <>
       <div
-        className="absolute top-4 left-1/2 -translate-x-1/2 z-50 
+        className="absolute top-10 left-1/2 -translate-x-1/2 z-50 
                ]px-4 py-2 select-none"
       >
         Tap on screen to jump
